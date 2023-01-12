addfb010591eea6e9bba8235b590117adbd41e9b
Mudit Kapoor <muditk441@gmail.com>
2023-01-12 09:53:35 +0530

    Problem solved

--- Starters 73 Contest/Consecutive Xor/solution.cpp
+++ Starters 73 Contest/Consecutive Xor/solution.cpp
@@ -1,50 +1,29 @@
-
-// C++ program for the above approach
-
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
-
 
 int main() {
 	// your code goes here
-
-	 int t;
-	 cin>>t;
-	 
-	 while(t--){
-	     int n;
-	     cin>>n;
-	     int a[n];
-	     
-	     for(int i=0;i<n;i++)
-	        cin>>a[i];
-	        
-	    for(int i=1;i<n;i++){
-	        a[i]=a[i]^a[i-1];
-	        a[i-1]=0;
-	    }
-	    
-	    int temp = n;
-	    for(int i=0; i< n ;i++){
-	        if(a[i]!=0)
-	            temp--;
-	    }
-	    if(n == temp){
-	    	        cout<<"YES"<<endl;
-	    	        continue;
-	    }
-	    if(n-1 == temp){
-	        if(temp % 2==0)
-	            cout<<"YES"<<endl;
-	        else
-	            cout<<"NO"<<endl;
-	    continue;
-	    }
-	    cout<<"NO"<<endl;
-
-	    
-	 }
+	int t;
+	cin>>t;
 	
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++){
+	        cin>>arr[i];
+	    }
+	    int xorOp=0
+	    for(int i =0 ;i<n;i++){
+	        xorOp^=arr[i];
+	    }
+
+	    if(xorOp == 0 || n % 2==1){
+	        cout<<"Yes"<<endl;
+	    }
+	    else
+	        cout<<"No"<<endl;
+	    
+	}
 	return 0;
 }

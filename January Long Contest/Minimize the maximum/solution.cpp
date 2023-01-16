4aac74904f3e364b4379e6c81b20773de6681e6d
Mudit Kapoor <muditk441@gmail.com>
2023-01-16 23:22:07 +0530

    Updated approach

--- January Long Contest/Minimize the maximum/solution.cpp
+++ January Long Contest/Minimize the maximum/solution.cpp
@@ -1,61 +1,28 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-#include<vector>
-#include<climits>
-#include<algorithm>
 
-#define ll long long
-
-bool solve(int n , vector<ll> arr ,ll mid)
-{
-    vector<ll> newArray =arr;
-    if(newArray[0] > mid){
-        return false;
-    }
-    for(int i = 1 ; i < n ; i++){
-        if(newArray[i-1] < mid)
-    }
-    
-    
-    
-    
-    return true;
-    
-}
 int main() {
-	// your code goes here
-	int t;
-	
-	cin>>t;
-	
-	while(t--){
-	    int n;
-	    cin>>n;
-	    
-	    vector<ll> arr[n];
-	    for(int i=0 ; i < n ;i++)
-	        cin>>arr[i];
-	   //starting number
-	    ll  i = 0;
-	   // ending number
-	    ll  j = *max_element(arr.begin(),arr.end());
-	    
-	    ll ans = LONG_MAX;
-	    
-	    while( i <= j){
-	        ll mid = i + (j-i) / 2;
-	        
-	        if(solve(n , arr ,mid )){
-	            ans = min(ans,mid);
-	            j = mid - 1;
-	        }
-	        else{
-	            i = mid + 1;
-	        }
-	    }
-	    
-	    cout<<ans<<endl;
-	}
-	
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        
+        // calculate average for all indexes and take out max out of it
+        int sum = 0 , ans=0 ,avg=0;
+        for(int i=1;i<=n;i++){
+            sum +=a[i-1];
+            if(sum%i == 0)
+                avg=sum/i;
+            else
+                avg=sum/i + 1;
+            ans = max(ans,avg);
+        }
+        cout<<ans<<endl;
+    }
 	return 0;
 }
